cc9d1a3810ee56779ea258a7c517a4572fd1db4f
Kako Botasso <kakodev@gmail.com>
2017-08-30 00:56:13 -0300

    Refatora router

--- config/routes.go
+++ config/routes.go
@@ -3,9 +3,7 @@ package config
 import (
 	"net/http"
 
-	"github.com/gorilla/mux"
 	"github.com/kakobotasso/watermanager/handlers"
-	"github.com/kakobotasso/watermanager/models"
 )
 
 type Route struct {
@@ -16,26 +14,6 @@ type Route struct {
 }
 
 type Routes []Route
-
-func NewRouter() *mux.Router {
-
-	router := mux.NewRouter().StrictSlash(true)
-	for _, route := range routes {
-
-		var handler http.Handler
-
-		handler = route.HandlerFunc
-		handler = models.Logger(handler, route.Name)
-
-		router.
-			Methods(route.Method).
-			Path(route.Pattern).
-			Name(route.Name).
-			Handler(handler)
-	}
-
-	return router
-}
 
 var routes = Routes{
 	Route{
