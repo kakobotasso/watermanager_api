92959f2e68422f413af9c1848e4e06f6ac1b63b4
Kako Botasso <kakodev@gmail.com>
2017-08-29 22:23:21 -0300

    Adiciona Gorilla Mux

--- main.go
+++ main.go
@@ -5,12 +5,16 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	log.Fatal(http.ListenAndServe(":3000", router))
+}
 
-	log.Fatal(http.ListenAndServe(":3000", nil))
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
