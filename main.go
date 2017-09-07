ca658d78ace39bc626c4fb0b3839dfc2d64cb31e
Kako Botasso <kakodev@gmail.com>
2017-09-07 01:52:57 -0300

    Usa porta configurada no servidor

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/kakobotasso/watermanager/config"
 )
 
 func main() {
+	port := fmt.Sprintf(":%s", os.Getenv("PORT"))
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	router := config.NewRouter()
-	log.Fatal(http.ListenAndServe(":3000", router))
+	log.Fatal(http.ListenAndServe(port, router))
 }
